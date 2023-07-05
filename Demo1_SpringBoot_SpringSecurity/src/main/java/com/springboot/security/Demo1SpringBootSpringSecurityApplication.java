66409228d5fbb8c77b11f277cf393c84ff17d5df
Ranju <ranjupillai2016@gmail.com>
2023-07-05 17:56:47 +0530

    Initial commit

---
+++ Demo1_SpringBoot_SpringSecurity/src/main/java/com/springboot/security/Demo1SpringBootSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringBootSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpringBootSpringSecurityApplication.class, args);
+	}
+
+}
